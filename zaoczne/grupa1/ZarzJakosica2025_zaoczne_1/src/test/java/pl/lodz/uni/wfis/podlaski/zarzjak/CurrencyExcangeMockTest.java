b896f19f257ded2948059e531ff22bbcae96fd00
kpodlaski <podlaski@uni.lodz.pl>
2025-03-30 11:32:46 +0200

    start mockito tests

--- zaoczne/grupa1/ZarzJakosica2025_zaoczne_1/src/test/java/pl/lodz/uni/wfis/podlaski/zarzjak/CurrencyExcangeMockTest.java
+++ zaoczne/grupa1/ZarzJakosica2025_zaoczne_1/src/test/java/pl/lodz/uni/wfis/podlaski/zarzjak/CurrencyExcangeMockTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class CurrencyExchangeTest {
+public class CurrencyExcangeMockTest {
 
     private CurrencyExchange testTarget;
     private CurrencyRates rates;
@@ -18,7 +18,7 @@ class CurrencyExchangeTest {
     @BeforeEach
     void setUp() {
         //System.out.println("SETIING UP");
-        rates = new CurrencyRates();
+        rates = mock(CurrencyRates.class);
         testTarget = new CurrencyExchange(rates);
     }
 
@@ -29,34 +29,19 @@ class CurrencyExchangeTest {
     }
 
     @Test
-    void exchange() throws Exception {
+    void mockExchange() throws Exception {
+        //w obiekcie testTarget, podmieniamy rates na obiekt mock
+        //CurrencyRates rates1 = mock(CurrencyRates.class);
+        //Field rateField = CurrencyExchange.class.getDeclaredField("rates");
+        //rateField.setAccessible(true);
+        //rateField.set(testTarget, rates1);
+        //rateField.setAccessible(false);
         //System.out.println("Test 1");
         //GIVEN PLN->EUR rate = .33
-        rates.setRate(Currency.PLN, Currency.EUR, .33);
+        when(rates.getRate(Currency.PLN,Currency.EUR)).thenReturn(.33);
         //WHEN PLN->EUR, amount = 100
         double result = testTarget.exchange(Currency.PLN, Currency.EUR,100);
         //THEN result = 25 euro
         assertEquals(.33*100,result,.01,"Bad exchange result");
-    }
-
-    @Test
-    void exchange2() {
-        //System.out.println("Test 2");
-        //GIVEN rates for PLN->USD is not set
-        //WHEN PLN->USD, amount = 100
-        //THEN Exception is thrown
-        assertThrows(Exception.class, ()->{
-            double result = testTarget.exchange(Currency.PLN, Currency.USD,100);
-        });
-    }
-
-    @Test
-    void exchange3() throws Exception {
-        //System.out.println("Test 2");
-        //GIVEN no requirements
-        //WHEN EUR->EUR, amount = 100
-        double result = testTarget.exchange(Currency.EUR, Currency.EUR,100);
-        //THEN result 100 euro
-        assertEquals(100,result,.01);
     }
 }
