fd1ac4003461dcd244014bed3193407161e653c8
kpodlaski <podlaski@uni.lodz.pl>
2025-06-01 16:56:09 +0200

    selenium base

--- zaoczne/grupa2/Selenium/src/test/java/seleniumTests/WWWSeleniumTest.java
+++ zaoczne/grupa2/Selenium/src/test/java/seleniumTests/WWWSeleniumTest.java
@@ -14,19 +14,36 @@ public class WWWSeleniumTest {
 
 
     @Test
-    public void basicTest() {
+    public void basicTest() throws InterruptedException {
         //Test Based on:
         //https://www.selenium.dev/documentation/webdriver/getting_started/first_script/
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
         driver.getTitle();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        Thread.sleep(500);
         WebElement textBox = driver.findElement(By.name("my-text"));
         WebElement submitButton = driver.findElement(By.cssSelector("button"));
         textBox.sendKeys("Selenium");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+        Thread.sleep(2000);
         submitButton.click();
         WebElement message = driver.findElement(By.id("message"));
         assertEquals("Received!",message.getText());
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+        Thread.sleep(2000);
         driver.quit();
+    }
+
+    @Test
+    public void imageOverButton() throws InterruptedException {
+        String url ="D:\\git\\Zajecia\\ZarzadzanieJakoscia2024-25\\zaoczne\\grupa2\\Selenium\\src\\test\\resources\\simple.html";
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        WebElement message = driver.findElement(By.id("btn"));
+        message.click();
+        Thread.sleep(2000);
+        driver.quit();
+
     }
 }
